530b8a528493442b5b4f89f7ed25c5569b162f13
zjria <zakaria.com.bd@gmail.com>
2024-01-17 17:32:54 +0600

    done

--- js/app.js
+++ js/app.js
@@ -16,6 +16,48 @@ function navBartoggleBtn() {
 
 navBartoggleBtn();
 
+// owl carousel js
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        autoplay: true,
+        smartSpeed: 10,
+        dots: false,
+        navText: [
+            '<i class="fa-solid fa-angle-left"></i>',
+            '<i class="fa-solid fa-angle-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false,
+                navText: false,
+            },
+            576: {
+                items: 1,
+                dots: true,
+                nav: false,
+                navText: false,
+            },
+            768: {
+                items: 1,
+                dots: true,
+                nav: false,
+                navText: false,
+            },
+            992: {
+                items: 1,
+                // dots: true,
+                // nav: false,
+                // navText: false,
+            }
+        }
+    })
+});
+
+
 // Aos Animation
 AOS.init({
     offset: 120, // offset (in px) from the original trigger point
@@ -28,23 +70,38 @@ AOS.init({
 
 });
 
-// owl carousel js
-$(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        items: 1,
-        autoplay: false,
-        navText: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 1,
-            },
-            1000: {
-                items: 1,
-            }
-        }
-    })
-});
+
+// form validation
+
+// const form = document.getElementById('form');
+// const fname = document.getElementById('fname');
+// const lName = document.getElementById('lName');
+// const email = document.getElementById('email');
+// const error = document.getElementById('error');
+
+
+// fname.addEventListener('mouseover', () => {
+//     fname.classList.add('border-style-add');
+
+// });
+// fname.addEventListener('click', () => {
+//     fname.classList.add('border-color');
+// });
+
+
+// form.addEventListener('submit', (e) => {
+//     let message = [];
+//     if (fname.value === '' || fname.value === null) {
+//         message.push('Type your name');
+//         fname.classList.add('border-color');
+//     };
+
+
+//     if (message.length > 0) {
+//         e.preventDefault();
+//         error.innerText = message.join(', ');
+//     };
+
+//     console.log('addd');
+// });
+
